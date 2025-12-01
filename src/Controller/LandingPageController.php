b5f18dbf2c2f5ff5de4e269a57c8647defacfbe9
Kramz <marklovegames55@gmail.com>
2025-12-01 22:43:10 +0800

    added landing page for desnsyst

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController extends AbstractController
+class LandingPageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
